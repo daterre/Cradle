60bc72c38e2f371f1a996eb8cfc843fd2f64a1db
D. A. Terre <daterre@Ds-MacBook-Pro.local>
2017-06-20 07:52:01 +0300

    Fix for mac compilation error. #25 tested on Mac

--- Assets/Cradle/Editor/ThirdParty/PhantomJS/Install.cs
+++ Assets/Cradle/Editor/ThirdParty/PhantomJS/Install.cs
@@ -12,7 +12,7 @@ public static class Install_PhantomJS {
     {
         Process chmod = new Process();
         chmod.StartInfo.UseShellExecute = false;
-		chmod.StartInfo.WorkingDirectory = PhantomJS.FindFile("phantomjs", true);
+        chmod.StartInfo.WorkingDirectory = EditorFileUtil.FindFile("phantomjs", true);
         chmod.StartInfo.FileName = "chmod";
         chmod.StartInfo.Arguments = "+x phantomjs";
         chmod.StartInfo.RedirectStandardOutput = true;
