0fe431fa7428821a6f2ad7bbae7732487ddb2cfb
daterre <doron.t@eyeway-vision.com>
2017-11-27 00:39:18 +0200

    Updated assembly versions and preparing for release

--- .src/Cradle/Properties/AssemblyInfo.cs
+++ .src/Cradle/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
 #endif
